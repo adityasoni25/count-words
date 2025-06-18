936c0d5c882c6469843c85d10c08774d03076722
Aditya Soni <adityasoni252@gmail.com>
2025-06-18 17:58:50 +0530

    Initial commit: Count Words app

---
+++ src/main/java/com/example/countwords/CountwordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.countwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/countwords/CountwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

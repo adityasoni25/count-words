18ac28fee752a6197eb0bfc456bef016105d939a
Aditya Soni <adityasoni252@gmail.com>
2025-06-19 21:57:43 +0530

    sonar fixes

--- src/main/java/com/example/countwords/service/FileWordReader.java
+++ src/main/java/com/example/countwords/service/FileWordReader.java
@@ -1,24 +1,33 @@
 package com.example.countwords.service;
 
+import com.example.countwords.exceptions.WordFileReadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 public class FileWordReader {
 
-    public List<String> readWords() throws Exception {
+    @Value("${words.file-path}")
+    Resource wordsFile;
+
+    public List<String> readWords() {
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(this.getClass().getResourceAsStream("/words.txt")))) {
+                new InputStreamReader(wordsFile.getInputStream()))) {
 
             return reader.lines()
                     .flatMap(line -> Stream.of(line.trim().split("\\s+")))
                     .filter(word -> !word.isBlank())
-                    .collect(Collectors.toList());
+                    .toList();
+
+        } catch (IOException e) {
+            throw new WordFileReadException("Failed to read words from file", e);
         }
     }
 
--- src/test/java/com/example/countwords/service/FileWordReaderTest.java
+++ src/test/java/com/example/countwords/service/FileWordReaderTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FileWordReaderTest {
+class FileWordReaderTest {
 
     private FileWordReader fileWordReader;
 
